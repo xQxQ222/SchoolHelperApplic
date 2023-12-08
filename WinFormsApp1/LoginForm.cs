123c7da0e9b1d0c47a4f525f0d93f9e8579d0122
Irodenko Anastasia <152500414+JustNairo@users.noreply.github.com>
2023-12-08 19:38:08 +0400

    Add files via upload

--- WinFormsApp1/LoginForm.cs
+++ WinFormsApp1/LoginForm.cs
@@ -1,4 +1,4 @@
-﻿using MySql.Data.MySqlClient;
+﻿using MySqlConnector;
 using System.Data;
 
 namespace WinFormsApp1
@@ -15,47 +15,19 @@ namespace WinFormsApp1
             String login = LoginField.Text;
             String password = PasswordField.Text;
 
-            DB db = new DB();
-            DataTable dt = new DataTable();
+            bool userIsLoggedInSuccessfully = ReadFromDB.ReadCurrentUser(login, password); // выглядит нелогично
 
-            MySqlDataAdapter adapter = new MySqlDataAdapter();
-
-            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login`=@uL AND `password`=@uP", db.getConnection());
-            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = login;
-            command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = password;
-
-            adapter.SelectCommand = command;
-            adapter.Fill(dt);
-            db.openConnection();
-            if (dt.Rows.Count > 0)
+            if (userIsLoggedInSuccessfully)
             {
                 MessageBox.Show("Пользователь успешно авторизован");
                 Thread.Sleep(500);
 
-                var read = command.ExecuteReader();
-                read.Read();
-
-                var withoutTime = read.GetString(6).Split(' ')[0];
-                var day = int.Parse(withoutTime.Split('.')[0]);
-                var month = int.Parse(withoutTime.Split('.')[1]);
-                var year = int.Parse(withoutTime.Split(".")[2]);
-                var date = new DateTime(year, month, day);
-                currentUser._id = int.Parse(read.GetString(0));
-                currentUser._login = read.GetString(1);
-                currentUser._password = read.GetString(2);
-                currentUser._name = read.GetString(3);
-                currentUser._surename = read.GetString(4);
-                currentUser._patronymic = read.GetString(5);
-                currentUser._Date = date;
-                currentUser._status = read.GetString(7);
-                currentUser._email = read.GetString(8);
                 var form = new Menu();
                 form.Show();
                 this.Close();
             }
             else
                 MessageBox.Show("Неправильный логин или пароль");
-            db.closeConnection();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
